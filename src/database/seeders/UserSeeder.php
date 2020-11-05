57d9f2aedff7e8640a509db3624987dc973c5e99
Omar Iriskic <noisewavehd@gmail.com>
2020-11-05 02:06:41 +0100

    Added UserSeeder

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()
+			->times(1)
+			->create([
+				'name' => 'Omar Iriskic',
+				'email' => 'me@omaririskic.com',
+				'password' => Hash::make('omar1234'),
+				'email_verified_at' => now()
+			]);
+    }
+}
