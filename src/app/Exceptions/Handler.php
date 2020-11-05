da68a5706da1d275df9c6c07ffda5fca98246c76
Omar Iriskic <noisewavehd@gmail.com>
2020-11-05 02:03:47 +0100

    Remvoed unnecessary commented out method

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -37,47 +37,6 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        //
+
     }
-
-    /*
-	public function render($request, Throwable $e)
-	{
-		// Currently converts AuthorizationException to 403 HttpException
-		// and ModelNotFoundException to 404 NotFoundHttpException
-		$exception = $this->prepareException($e);
-		// Default response
-		$response = [
-			'error' => 'Sorry, something went wrong.'
-		];
-
-		// Add debug info if app is in debug mode
-		if (config('app.debug')) {
-			// Add the exception class name, message and stack trace to response
-			$response['exception'] = get_class($exception); // Reflection might be better here
-			$response['message'] = $exception->getMessage();
-			$response['trace'] = $exception->getTrace();
-		}
-
-		$status = 400;
-		// Build correct status codes and status texts
-		switch ($exception) {
-			case $exception instanceof ValidationException:
-				return $this->convertValidationExceptionToResponse($exception, $request);
-			case $exception instanceof AuthenticationException:
-				$status = 401;
-				$response['error'] = Response::$statusTexts[$status];
-				break;
-			case $this->isHttpException($exception):
-				$status = $exception->getStatusCode();
-				$response['error'] = Response::$statusTexts[$status];
-				break;
-			default:
-				break;
-		}
-
-		return response()->json($response, $status);
-	}
-	*/
-
 }
