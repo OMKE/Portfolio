45674febffb9d27517900222a3d144c0f15ee914
Omar Iriskic <noisewavehd@gmail.com>
2020-11-18 00:49:28 +0100

    Added CreateUser console command

---
+++ src/app/Console/Commands/CreateUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = $this->ask("Name");
+        $email = $this->ask("Email");
+        $password = \Hash::make($this->secret("Password"));
+
+        User::factory()->create(compact('name', 'email', 'password'));
+
+        $this->info("User with email: " . $email . " has been created.");
+    }
+}
