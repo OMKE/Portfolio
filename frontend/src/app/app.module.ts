87b2424fe32bcdfb6768351bf7b3fd6e03855096
Omar Iriskic <omaririskic.dev@gmail.com>
2020-12-07 00:01:46 +0100

    Downgraded angular version to 10.0.0
    Ngrx is still unsupported for angular version 11, it causes conflicts in dependencies

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,14 @@
+import { SharedModule } from './shared/shared.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
