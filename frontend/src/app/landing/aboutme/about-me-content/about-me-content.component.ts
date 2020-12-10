b3f5e0175929716b89ee3356b9551839a5fbd2d9
Omar Iriskic <omaririskic.dev@gmail.com>
2020-12-10 21:37:46 +0100

    Added about me content component

---
+++ frontend/src/app/landing/aboutme/about-me-content/about-me-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me-content',
+  templateUrl: './about-me-content.component.html',
+  styleUrls: ['./about-me-content.component.scss']
+})
+export class AboutMeContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
