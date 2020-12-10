c05c4c54d78ea99f015509a539d420c34f7dcdd4
Omar Iriskic <omaririskic.dev@gmail.com>
2020-12-10 21:11:08 +0100

    Fixed dependencies

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,19 +12,31 @@ import { environment } from '../environments/environment';
 import { IndexComponent } from './landing/index/index.component';
 import { HeaderComponent } from './landing/index/header/header.component';
 import { AboutmeComponent } from './landing/aboutme/aboutme.component';
+import { ExperienceComponent } from './landing/index/experience/experience.component';
+import { ExperienceItemComponent } from './landing/index/experience/experience-item/experience-item.component';
+import { ContactComponent } from './landing/index/contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EffectsModule } from '@ngrx/effects';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     HeaderComponent,
+    AboutmeComponent,
+    ExperienceComponent,
+    ExperienceItemComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     SharedModule,
     StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
