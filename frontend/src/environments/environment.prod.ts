d836c393319adc652353868153ea73fb4e732216
Omar Iriskic <omaririskic.dev@gmail.com>
2020-12-18 05:57:50 +0100

    Added appName to env vars

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.omaririskic.com/api/v1'
+  apiUrl: 'https://api.omaririskic.com/api/v1',
+  appName: 'Omar Iriskic'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8088/api/v1'
+  apiUrl: 'http://localhost:8088/api/v1',
+  appName: 'Omar Iriskic'
 };
 
 /*
