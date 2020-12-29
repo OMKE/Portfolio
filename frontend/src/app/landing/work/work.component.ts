d496db48891c1ce95af11b589c6adae972a69246
Omar Iriskic <omaririskic.dev@gmail.com>
2020-12-30 00:11:26 +0100

    Set title to project name

--- frontend/src/app/landing/work/work.component.ts
+++ frontend/src/app/landing/work/work.component.ts
@@ -1,3 +1,6 @@
+import { tap } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
+import { setTitle } from './../../core/utilities/misc.utils';
 import { selectWorkImagesByWorkId, selectWorkImagesByWorkIdLoaded } from './../../core/store/work-image/work-image.selectors';
 import { loadWork } from './../../core/store/works/works.actions';
 import { selectWorkById, selectWorkByIdLoaded } from './../../core/store/works/works.selectors';
@@ -15,7 +18,7 @@ import { WorkImage } from 'src/app/core/store/work-image/work-image.model';
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.scss']
 })
-export class WorkComponent implements OnInit {
+export class WorkComponent implements OnInit{
 
   work$: Observable<Work>;
   images$: Observable<WorkImage[]>;
@@ -24,16 +27,33 @@ export class WorkComponent implements OnInit {
 
   loadedImages$: Observable<boolean>;
 
-  constructor(private store: Store<AppState>, private route: ActivatedRoute) { }
+  constructor(private store: Store<AppState>, private route: ActivatedRoute, private title: Title) { }
+
 
   ngOnInit(): void {
+    setTitle(this.title, '');
+
     const workId = this.route.snapshot.params.id;
+
     this.store.dispatch(loadWork({ workId}));
     this.store.dispatch(loadWorkImages( { workId }));
     this.work$ = this.store.pipe(select(selectWorkById(workId)));
     this.images$ = this.store.pipe(select(selectWorkImagesByWorkId(workId)));
     this.loaded$ = this.store.pipe(select(selectWorkByIdLoaded(workId)));
     this.loadedImages$ = this.store.pipe(select(selectWorkImagesByWorkIdLoaded(workId)));
+
+
+    this.loaded$.subscribe(res => {
+      if (res === false) {
+        setTitle(this.title, `Work #${this.route.snapshot.params.id}`);
+      } else {
+        this.work$.subscribe(res => {
+          setTitle(this.title, res.title);
+        });
+      }
+    });
   }
+
+
 
 }
