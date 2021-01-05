0a6717cac55eb38c13acb09c7e7cd7c4c91edc6d
Omar Iriskic <omaririskic.dev@gmail.com>
2021-01-05 21:40:07 +0100

    Navbar hides on scroll

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -14,7 +14,11 @@ export class NavbarComponent implements OnInit {
 
   // invertedColor: boolean = true;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    document.addEventListener('scroll' , e => {
+      this.show = false;
+    });
+  }
 
 
   toggle() {
