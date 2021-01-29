6c9dd561caafc678aba3b86a9ca969d57495c641
Omar Iriskic <omaririskic.dev@gmail.com>
2021-01-29 22:50:22 +0100

    Added additional actions

--- frontend/src/app/auth/auth.actions.ts
+++ frontend/src/app/auth/auth.actions.ts
@@ -1,6 +1,7 @@
 import { LoginResponseSuccess, LoginResponseFailure } from './models/auth.response';
 import { LoginRequest } from './models/auth.request';
 import { createAction, props } from '@ngrx/store';
+import { User } from './models/user.model';
 
 export const requestAuth = createAction(
   '[Auth] Request Auth',
@@ -15,4 +16,26 @@ export const requestAuthSuccess = createAction(
 export const requestAuthFailure = createAction(
   '[Auth] Request Auth Failure',
   props<{ error: LoginResponseFailure }>()
+);
+
+export const requestAuthSuccessRedirect = createAction(
+  '[Auth] Request Auth Success Redirect',
+  props<{ route: string }>()
+);
+
+export const requestUser = createAction(
+  '[Auth] Request User'
+);
+
+export const requestUserSuccess = createAction(
+  '[Auth] Request User Success',
+  props<{data: User}>()
+);
+export const requestUserFailure = createAction(
+  '[Auth] Request User Failure',
+  props<{error: string}>()
+);
+
+export const requestAuthLogout = createAction(
+  '[Auth] Request Auth Logout'
 );
