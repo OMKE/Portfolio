59f4c5a767b4b349bdda3f2460a7b0f599cd6f32
Omar Iriskic <omaririskic.dev@gmail.com>
2021-01-29 22:53:59 +0100

    Updated Auth module

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,12 +1,37 @@
+import { AuthGuard } from './auth.guard';
+import { AuthInterceptor } from './auth.interceptor';
+import { SharedModule } from './../shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+import * as fromAuth from './auth.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './auth.effects';
 
+
+
+const AuthInterceptorProvider = {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+};
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AuthComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    SharedModule,
+    StoreModule.forFeature(fromAuth.authFeatureKey, fromAuth.reducer),
+    EffectsModule.forFeature([AuthEffects]),
+  ],
+  exports: [AuthComponent],
+  providers: [AuthInterceptorProvider, AuthGuard]
+
 })
 export class AuthModule { }
