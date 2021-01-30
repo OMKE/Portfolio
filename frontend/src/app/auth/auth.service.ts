8bb4fd73472f24faab4305ecd8eddac53b5da37d
Omar Iriskic <omaririskic.dev@gmail.com>
2021-01-30 22:01:22 +0100

    Added clearLS method for clearing all keys in localStorage

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { User } from './models/user.model';
 
 
 const TOKEN_KEY = 'oi';
+const USER_KEY = 'user';
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +23,7 @@ export class AuthService {
   login(data: LoginRequest): Observable<LoginResponseSuccess | LoginResponseFailure> {
     return this.http.post<LoginResponseSuccess|LoginResponseFailure>(getUrl('auth/login'), data);
   }
-  logout(): Observable<any> {
+  logout(): Observable<{message: string}> {
     return this.http.post<any>(getUrl('auth/logout'), null);
   }
 
@@ -31,11 +32,11 @@ export class AuthService {
   }
 
   getUserFromLS(): User {
-    return JSON.parse(localStorage.getItem('user'));
+    return JSON.parse(localStorage.getItem(USER_KEY));
   }
 
   setUserToLS(user: User): void {
-    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
   }
 
   setToken(response: LoginResponseSuccess): void {
@@ -56,12 +57,19 @@ export class AuthService {
   }
 
   refreshToken(): Observable<LoginResponseSuccess | LoginResponseFailure> {
-    return this.http.post<LoginResponseSuccess | LoginResponseFailure>(getUrl('auth/refreh'), null);
+    return this.http.post<LoginResponseSuccess | LoginResponseFailure>(getUrl('auth/refresh'), null);
   }
 
   removeToken(): void {
+    this.token = null;
     localStorage.removeItem(TOKEN_KEY);
   }
+  removeUserLS(): void {
+    localStorage.removeItem(USER_KEY);
+  }
 
-
+  clearLS(): void {
+    this.removeToken();
+    this.removeUserLS();
+  }
 }
