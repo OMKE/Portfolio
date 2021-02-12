b86747a8fdba4c9ca8b75662851aa5a7664e2084
Omar Iriskic <omaririskic.dev@gmail.com>
2021-02-12 23:08:55 +0100

    Changed icon colors

--- frontend/src/app/shared/components/oval-loader/oval-loader.component.ts
+++ frontend/src/app/shared/components/oval-loader/oval-loader.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-oval-loader',
   templateUrl: './oval-loader.component.html',
-  styleUrls: ['./oval-loader.component.scss']
+  styleUrls: ['./oval-loader.component.scss'],
 })
 export class OvalLoaderComponent implements OnInit {
+  @Input() invert = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- frontend/src/app/shared/components/status-icon/status-icon.component.ts
+++ frontend/src/app/shared/components/status-icon/status-icon.component.ts
@@ -4,17 +4,16 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-status-icon',
   templateUrl: './status-icon.component.html',
-  styleUrls: ['./status-icon.component.scss']
+  styleUrls: ['./status-icon.component.scss'],
 })
 export class StatusIconComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() invert = false;
 
   @Input() success: Observable<boolean>;
 
   @Input() error: Observable<boolean>;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
