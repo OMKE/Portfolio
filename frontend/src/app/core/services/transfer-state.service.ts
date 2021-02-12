4ee446f9f3bb40558693ba039c2c7e20303a0453
Omar Iriskic <omaririskic.dev@gmail.com>
2021-02-12 18:37:44 +0100

    Formatted

--- frontend/src/app/core/services/transfer-state.service.ts
+++ frontend/src/app/core/services/transfer-state.service.ts
@@ -1,14 +1,20 @@
 import { environment } from './../../../environments/environment';
 import { isPlatformServer } from '@angular/common';
-import { StateKey, TransferState, makeStateKey } from '@angular/platform-browser';
+import {
+  StateKey,
+  TransferState,
+  makeStateKey,
+} from '@angular/platform-browser';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TransferStateService {
-
-  constructor(@Inject(PLATFORM_ID) private readonly platformId, private readonly transferState: TransferState) { }
+  constructor(
+    @Inject(PLATFORM_ID) private readonly platformId,
+    private readonly transferState: TransferState
+  ) {}
 
   private keys = new Map<string, StateKey<string>>();
 
@@ -16,7 +22,10 @@ export class TransferStateService {
     if (!this.has(key)) {
       return defaultValue || null;
     }
-    const value = this.transferState.get<T>(this.getStateKey(key), defaultValue);
+    const value = this.transferState.get<T>(
+      this.getStateKey(key),
+      defaultValue
+    );
     return value;
   }
 
@@ -27,7 +36,6 @@ export class TransferStateService {
     this.transferState.remove(this.getStateKey(key));
   }
 
-
   has(key: string): boolean {
     return this.transferState.hasKey(this.getStateKey(key));
   }
@@ -35,7 +43,9 @@ export class TransferStateService {
   set<T>(key: string, value: T): void {
     if (isPlatformServer(this.platformId)) {
       if (this.has(key)) {
-        console.warn(`Setting existing value into TransferState using key: ${key}`);
+        console.warn(
+          `Setting existing value into TransferState using key: ${key}`
+        );
       }
       if (!environment.production) {
         console.log(`Storing TransferState for: ${key}`);
@@ -43,7 +53,6 @@ export class TransferStateService {
       this.transferState.set(this.getStateKey(key), value);
     }
   }
-
 
   private getStateKey(key: string): StateKey<string> {
     if (this.keys.has(key)) {
