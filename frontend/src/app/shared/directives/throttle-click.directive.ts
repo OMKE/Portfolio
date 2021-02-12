68d1e1feb6bd0f40095605347e3370e02d874440
Omar Iriskic <omaririskic.dev@gmail.com>
2021-02-12 22:40:12 +0100

    Update throttle-click.directive.ts

--- frontend/src/app/shared/directives/throttle-click.directive.ts
+++ frontend/src/app/shared/directives/throttle-click.directive.ts
@@ -1,4 +1,12 @@
-import { Directive, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+  Directive,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Subject, Subscription } from 'rxjs';
 import { throttleTime } from 'rxjs/operators';
 
@@ -6,10 +14,9 @@ import { throttleTime } from 'rxjs/operators';
   This directive stops user from multiple form submits, it fires one event and throttles others for default 2s
 */
 @Directive({
-  selector: '[appThrottleClick]'
+  selector: '[appThrottleClick]',
 })
 export class ThrottleClickDirective implements OnInit, OnDestroy {
-
   @Input() throttleTime = 2000;
 
   @Output() throttleClick = new EventEmitter();
@@ -17,32 +24,38 @@ export class ThrottleClickDirective implements OnInit, OnDestroy {
   private clicks = new Subject();
   private subscription: Subscription;
 
-  constructor() { }
-
+  constructor() {}
 
   ngOnInit(): void {
-    this.subscription = this.clicks.pipe(
-      throttleTime(this.throttleTime)
-    ).subscribe(e => this.throttleClick.emit(e));
+    this.subscription = this.clicks
+      .pipe(throttleTime(this.throttleTime))
+      .subscribe((e) => this.throttleClick.emit(e));
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
   @HostListener('click', ['$event']) clickEvent(event): void {
-    if (!this.checkIfNotFormField(event))
-    {
-      console.log(event.target.tagName);
+    if (!this.checkIfNotFormField(event)) {
       event.preventDefault();
       event.stopPropagation();
       this.clicks.next(event);
     }
-
   }
 
   checkIfNotFormField(event: any): boolean {
-    const fields = ['INPUT', 'CHECKBOX', 'TEXTAREA', 'RADIO', 'SELECT', 'FIELDSET', 'OPTION', 'LEGEND', 'FORM', 'DIV'];
+    const fields = [
+      'INPUT',
+      'CHECKBOX',
+      'TEXTAREA',
+      'RADIO',
+      'SELECT',
+      'FIELDSET',
+      'OPTION',
+      'LEGEND',
+      'FORM',
+      'DIV',
+    ];
     return fields.includes(event.target.tagName);
   }
-
 }
