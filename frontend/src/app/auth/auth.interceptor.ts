6b43b53d9a1c481b697fc1fa8c4def276654368b
Omar Iriskic <noisewavehd@gmail.com>
2021-04-01 14:43:12 +0200

    Fixed auth interceptor

--- frontend/src/app/auth/auth.interceptor.ts
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -1,4 +1,9 @@
-import { LoginResponseSuccess, LoginResponseFailure } from './models/auth.response';
+import { requestAuthLogoutUser } from './auth.actions';
+import { Store } from '@ngrx/store';
+import {
+  LoginResponseSuccess,
+  LoginResponseFailure,
+} from './models/auth.response';
 import { tap, catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
@@ -8,31 +13,42 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpResponse
+  HttpResponse,
+  HttpErrorResponse,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AppState } from '../core/store';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private store: Store<AppState>
+  ) {}
 
-  constructor(private authService: AuthService, private router: Router) {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     const token = this.authService.getToken();
-    if (token !== null) {
-      const newRequest = request.clone({ headers: request.headers.set('Authorization', token) });
+    if (token) {
+      const newRequest = request.clone({
+        headers: request.headers.set('Authorization', token),
+      });
       return next.handle(newRequest).pipe(
-        tap(response => {
-          if (response instanceof HttpResponse) {
-            if (response.status === 401) {
-              // @TODO Should be tested
-              this.authService.refreshToken().pipe(
-                tap((res: LoginResponseSuccess) => this.authService.setToken(res))
-              ).subscribe();
+        tap(
+          () => {},
+          (err: any) => {
+            if (err instanceof HttpErrorResponse) {
+              if (err.status !== 401) {
+                return;
+              }
+              this.store.dispatch(requestAuthLogoutUser());
+              this.router.navigate(['auth']);
             }
           }
-        })
+        )
       );
     }
 
