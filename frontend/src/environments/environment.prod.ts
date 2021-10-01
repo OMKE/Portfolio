d35aa5905714dd8ea5205db93de323bdf12c2c20
Omar Iriskic <noisewavehd@gmail.com>
2021-10-01 21:50:59 +0200

    Fixed compilation bugs

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://api.omaririskic.com/api/v1',
-  apiUrl: 'http://localhost:8088/api/v1',
-  appName: 'Omar Iriskic'
+  apiUrl: 'https://api.omaririskic.com/api/v1',
+  // apiUrl: 'http://localhost:8000/api/v1',
+  appName: 'Omar Iriskic',
 };
